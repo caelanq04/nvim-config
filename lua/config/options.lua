09620256e159d0039219b8a660b4ddeccfa40ec9
Caelan Quigley <caelanq2021@gmail.com>
2024-12-12 11:03:47 +0000

    Updated options to configure cursor on startup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,12 @@
 -- Add any additional options here
 
 -- vim.opt.cursorlineopt = "number"
+--
+--
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.cmd(
+      "set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+    )
+  end,
+})
