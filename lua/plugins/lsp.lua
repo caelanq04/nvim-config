df33a05267c08d60756f28117fd323d0e5f79bd1
caelanq04 <caelanq2021@gmail.com>
2024-12-12 10:42:57 +0000

    Configuring pyright to work with ruff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,17 +3,27 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        pyright = {
-          mason = false,
-          autostart = false,
-        },
-        basedpyright = {
-          mason = true,
-          autostart = true,
-        },
         emmet_language_server = {
           mason = true,
           autostart = true,
+        },
+        pyright = {
+          capabilities = (function()
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+            return capabilities
+          end)(),
+          settings = {
+            python = {
+              analysis = {
+                useLibraryCodeForTypes = true,
+                diagnosticSeverityOverrides = {
+                  reportUnusedVariable = "warning", -- or anything
+                },
+                typeCheckingMode = "basic",
+              },
+            },
+          },
         },
         -- ruff = {
         -- mason = false,
